f67d4ec87ada4389e13a6c0e98586b62ed56c8a3
Daniel Hinkelmann <d.hinkelmann03@outlook.de>
2024-10-17 14:43:23 +0200

    refactor: package structure

---
+++ python/cubeRecognizer/threeXthree.py
@@ -0,0 +1,53 @@
+import cv2
+from matplotlib import pyplot as plt
+
+
+class CubeRecognizer:
+    def __init__(self, file):
+        self.file = file
+        self.images = []
+        self.cv2_images = []
+
+    def get_images(self):
+        with open(self.file) as f:
+            lines = f.readlines()
+            [self.images.append(line[:-1]) for line in lines]
+        return self.images
+
+    def set_cv2_images(self):
+        if not self.images:
+            raise AttributeError("Images have not been read yet.")
+
+        [self.cv2_images.append(cv2.cvtColor(cv2.imread(img), cv2.COLOR_BGR2RGB)) for img in self.images]
+
+    def recognize(self):
+        if not self.cv2_images:
+            raise AttributeError("Some lists have not been set yet")
+
+        edges = cv2.Canny(self.cv2_images[0], 100, 200)
+        contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(self.cv2_images[0], contours, -1, (0, 255, 0), 3)
+
+        for contour in contours:
+            x, y, w, h = cv2.boundingRect(contour)
+
+            if abs(w - h) < 10:
+                # cut out area of square
+                square = self.cv2_images[0][y:y + h, x:x + w]
+
+                # save the color of the square
+                color = cv2.mean(square)
+
+                # draw rectangle around the found square
+                cv2.rectangle(self.cv2_images[0], (x, y), (x + w, y + h), (255, 0, 0), 10)
+
+
+cr = CubeRecognizer('testPicsPathFIle')
+
+cr.get_images()
+cr.set_cv2_images()
+cr.recognize()
+
+plt.subplot(1, 1, 1)
+plt.imshow(cr.cv2_images[0])
+plt.show()
