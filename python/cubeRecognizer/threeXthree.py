fd0bbee068e1c64a724d31ef83c1f2f8a5d22c05
Dhtrx <d.hinkelmann03@outlook.de>
2024-10-22 11:13:34 +0200

    feat: tried to make recognition better -> unsuccessful

--- python/cubeRecognizer/threeXthree.py
+++ python/cubeRecognizer/threeXthree.py
@@ -7,6 +7,7 @@ class CubeRecognizer:
         self.file = file
         self.images = []
         self.cv2_images = []
+        self.gray_images = []
 
     def get_images(self):
         with open(self.file) as f:
@@ -20,15 +21,30 @@ class CubeRecognizer:
 
         [self.cv2_images.append(cv2.cvtColor(cv2.imread(img), cv2.COLOR_BGR2RGB)) for img in self.images]
 
-    def recognize(self):
+    def set_gray_images(self):
         if not self.cv2_images:
+            raise AttributeError("CV2Images have not been set yet.")
+
+        [self.gray_images.append(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)) for img in self.cv2_images]
+
+    def recognize(self):
+        if not self.gray_images:
             raise AttributeError("Some lists have not been set yet")
 
         edges = cv2.Canny(self.cv2_images[0], 100, 200)
         contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        min_area = 100
+
+        big_contours = []
+        for contour in contours:
+            area = cv2.contourArea(contour)
+            if area > min_area:
+                big_contours.append(contour)
+
         cv2.drawContours(self.cv2_images[0], contours, -1, (0, 255, 0), 3)
 
-        for contour in contours:
+        for contour in big_contours:
             x, y, w, h = cv2.boundingRect(contour)
 
             if abs(w - h) < 10:
@@ -46,6 +62,7 @@ cr = CubeRecognizer('testPicsPathFIle')
 
 cr.get_images()
 cr.set_cv2_images()
+cr.set_gray_images()
 cr.recognize()
 
 plt.subplot(1, 1, 1)
